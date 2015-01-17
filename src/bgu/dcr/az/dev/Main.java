702b66392ad36368bd0bd4cd19971721d10b0e1c
OliviaSu <oliviasu.w@gmail.com>
2015-01-17 23:53:53 +0800

    First commit Include: src and jar files from agentZero official website

---
+++ src/bgu/dcr/az/dev/Main.java
@@ -0,0 +1,20 @@
+package bgu.dcr.az.dev;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import bgu.dcr.az.api.exen.stat.Database;
+import bgu.dcr.az.exen.stat.db.DatabaseUnit;
+
+/**
+ * Created by ChrisQin on 10/7/2014.
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException, IOException {
+    	System.out.println("test");
+        ExperimentExecutionController.UNIT.run(new File("test.xml"), true, false);
+    }
+}
